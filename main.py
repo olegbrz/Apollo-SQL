778f204f98563a63fa6a9dc8efa7f0d947977a44
Oleg <brezitskyyoleg@gmail.com>
2020-05-30 02:22:56 +0200

    linked insert option to insert function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import core.ui
 import core.config
+import core.utils
 import cx_Oracle
 
 def connect_to_server():
@@ -38,7 +39,7 @@ if __name__ == "__main__":
             if not connected_to:
                 try:
                     print('\n [*] Connecting...')
-                    cursor = connect_to_server()
+                    CURSOR = connect_to_server()
                     host = settings['connection']['host']
                     port = settings['connection']['port']
                     user = settings['connection']['user']
@@ -47,7 +48,7 @@ if __name__ == "__main__":
                     print('\n [!] Connection failed, please check settings.')
             else:
                 try:
-                    cursor.close()
+                    CURSOR.close()
                     connected_to = ''
                     print('\n [i] Disconnected successfully.')
                 except: pass
@@ -56,9 +57,15 @@ if __name__ == "__main__":
         elif switch == 2: pass
         # TODO: [3] Alter data function
         elif switch == 3: pass
+        # [4] Isert data function
+        elif switch == 4:
+            if connected_to == '':
+                print('\n[!] Error, no connection was detected.')
+                continue
+            core.utils.insert(CURSOR)
 
         # [4] Settings option
-        elif switch == 4:
+        elif switch == 5:
             option = 1
             while option != 0:
                 option = core.ui.print_settings()
@@ -69,7 +76,7 @@ if __name__ == "__main__":
 
         # [0] Exit execution
         elif switch == 0:
-            try: cursor.close()
+            try: CURSOR.close()
             except: pass
             print('\n Good bye!\n')
             break
